2d09792015e660b303709629d08d2a49143d460c
zullf <fikarkeren20@gmail.com>
2026-04-28 11:03:25 +0700

    merefactor flow dari mealcard

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,25 +1,17 @@
 import React from "react";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet, } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function MealCard({ item, onPress }) {
   const image = item.strCategoryThumb || item.strMealThumb;
-  const title = item.strCategory || item.strMeal;
+  const title = item.strMeal || item.strCategory;
 
   return (
     <TouchableOpacity style={styles.card} onPress={onPress}>
       <Image source={{ uri: image }} style={styles.image} />
-
       <View style={styles.content}>
         <Text style={styles.title}>{title}</Text>
       </View>
-
       <Ionicons name="chevron-forward" size={20} color="#888" />
     </TouchableOpacity>
   );
@@ -36,17 +28,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     elevation: 3,
   },
-  image: {
-    width: 60,
-    height: 60,
-    borderRadius: 10,
-  },
-  content: {
-    flex: 1,
-    marginLeft: 10,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
+  image: { width: 60, height: 60, borderRadius: 10, },
+  content: { flex: 1, marginLeft: 10, },
+  title: { fontSize: 16, fontWeight: "bold", },
 });
